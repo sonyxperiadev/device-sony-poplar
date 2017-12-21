350e7ee28968c01d5b98268df1d92efccc146c56
James Muir <james.muir@graphitesoftware.com>
2017-12-21 11:35:13 -0500

    add logic to set TARGET_BOOTLOADER_BOARD_NAME
    Set TARGET_BOOTLOADER_BOARD_NAME to the variant-specific board-name
    (e.g. G8141, G8142).  This causes the "board" variable in
    "android-info.txt" to be populated correctly, which fixes
    updatepackage zips.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,7 +14,14 @@
 
 include device/sony/yoshino/PlatformConfig.mk
 
+TARGET_BOOTLOADER_BOARD_NAME := unknown
+ifneq (,$(filter %g8341,$(TARGET_PRODUCT)))
 TARGET_BOOTLOADER_BOARD_NAME := G8341
+else ifneq (,$(filter %g8342,$(TARGET_PRODUCT)))
+TARGET_BOOTLOADER_BOARD_NAME := G8342
+else
+$(error Unrecognized value for TARGET_PRODUCT: "$(TARGET_PRODUCT)")
+endif
 
 # Platform
 PRODUCT_PLATFORM := yoshino
